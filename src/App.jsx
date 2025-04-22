bab859227a1c3912a6dc58d72942c705752b46ca
Jon <jlee119955@gmail.com>
2025-04-22 12:26:00 -0400

    added our auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-  console.log(" App.jsx is working");
+  console.log("Working bro");
 
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+      <div>
+        <h1>Log In</h1>
+        <p>Welcome back! Traveler!</p>
+      </div>
+    );
+  }
