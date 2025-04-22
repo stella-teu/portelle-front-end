cc84371a69907788e3ebe456e2c691ed62bb8a4a
Jon <jlee119955@gmail.com>
2025-04-22 13:36:42 -0400

    added functions

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,15 @@
+import React from "react";
 export default function LoginPage() {
     return (
       <div>
         <h1>Log In</h1>
-        <p>Welcome back! Traveler!</p>
+        <p>Welcome back, Traveler!</p>
+        
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Log In</button>
+        </form>
       </div>
     );
   }
