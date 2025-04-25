1adc37da97cfba31f13ffebd8458099dcbb14b09
User Name <victoriamurra30@gmail.com>
2025-04-25 09:28:36 -0400

    createevent and evetnservice

---
+++ src/services/eventService.js
@@ -0,0 +1,30 @@
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/events`;
+
+const create = async (formData) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, //  Send JWT for auth
+
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error creating event:", error);
+    return { error: error.message };
+  }
+};
+
+
+
+
+export { create };
+
+
